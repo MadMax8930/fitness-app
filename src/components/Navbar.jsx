8a05bd8f526beef6ac9783caf5ccbdc5290161d2
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2023-06-13 22:34:06 +0200

    navbar & footer

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,18 @@
-const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+import { Link } from 'react-router-dom';
+import { Stack } from '@mui/material';
+
+import Logo from '../assets/images/Logo.png';
+
+const Navbar = () => (
+  <Stack direction="row" sx={{ gap: { sm: '123px', xs: '40px' }, mt: { sm: '32px', xs: '20px' }, px:'20px'}}>
+    <Link to="/">
+      <img src={Logo} alt="Logo" style={{ width: '48px', height: '48px', margin: '0px 20px' }} />
+    </Link>
+    <Stack direction="row" gap="40px" fontFamily="Arial" fontSize="24px" alignItems="flex-end">
+      <Link to="/" style={{ textDecoration: 'none', color: '#3A1212', borderBottom: '3px solid #FF2625' }}>Home</Link>
+      <a href="#exercises" style={{ textDecoration: 'none', color: '#3A1212' }}>Exercises</a>
+    </Stack>
+  </Stack>
+);
 
 export default Navbar;
