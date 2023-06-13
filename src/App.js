f3b0e1cc0770589e6a3eba1588fcca25484a9cfb
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2023-06-13 21:53:31 +0200

    setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import './index.css';
+import { Route, Routes } from 'react-router-dom';
+import { Box } from '@mui/material';
+
+import { Navbar, Footer } from './components';
+import { HomePage, ExercisePage } from './pages';
 
 const App = () => {
   return (
-    <div>App</div>
+      <Box width="400px" sx={{ width: { xl: '1488px' } }} m="auto">
+         <Navbar />
+         <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/exercise/:id" element={<ExercisePage />} />
+         </Routes>
+         <Footer />
+      </Box>
   )
 }
 
