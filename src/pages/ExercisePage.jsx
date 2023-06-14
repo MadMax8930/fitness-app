dcff0aab198bde3a3b27ab9ca3fcf0b7c025ffab
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2023-06-15 01:09:42 +0200

    exo page done

--- src/pages/ExercisePage.jsx
+++ src/pages/ExercisePage.jsx
@@ -1,7 +1,49 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { Box } from '@mui/material';
+
+import { fetchData, exerciseOptions, ytOptions } from '../utils/fetchData';
+import { ExoDetail, ExoVideos, SimilarExercises } from '../components';
+
 const ExercisePage = () => {
+  const [exerciseDetail, setExerciseDetail] = useState({});
+  const [exerciseVideos, setExerciseVideos] = useState([]);
+  const [targetMuscleExercises, setTargetMuscleExercises] = useState([]);
+  const [equipmentExercises, setEquipmentExercises] = useState([]);
+  const { id } = useParams();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+
+    const fetchExercisesData = async () => {
+      const exoDbUrl = 'https://exercisedb.p.rapidapi.com';
+      const ytSearchUrl = 'https://youtube-search-and-download.p.rapidapi.com';
+
+      const exerciseDetailData = await fetchData(`${exoDbUrl}/exercises/exercise/${id}`, exerciseOptions);
+      setExerciseDetail(exerciseDetailData);
+
+      const exerciseVideosData = await fetchData(`${ytSearchUrl}/search?query=${exerciseDetailData.name}`, ytOptions);
+      setExerciseVideos(exerciseVideosData.contents);
+
+      const targetMuscleExercisesData = await fetchData(`${exoDbUrl}/exercises/target/${exerciseDetailData.target}`, exerciseOptions);
+      setTargetMuscleExercises(targetMuscleExercisesData);
+
+      const equipmentExercisesData = await fetchData(`${exoDbUrl}/exercises/equipment/${exerciseDetailData.equipment}`, exerciseOptions);
+      setEquipmentExercises(equipmentExercisesData);
+    };
+
+    fetchExercisesData();
+  }, [id]);
+
+  if (!exerciseDetail) return <div>No Data</div>;
+
   return (
-    <div>ExercisePage</div>
-  )
-}
+    <Box sx={{ mt: { lg: '96px', xs: '60px' } }}>
+      <ExoDetail exerciseDetail={exerciseDetail} />
+      <ExoVideos exerciseVideos={exerciseVideos} name={exerciseDetail.name} />
+      <SimilarExercises targetMuscleExercises={targetMuscleExercises} equipmentExercises={equipmentExercises} />
+    </Box>
+  );
+};
 
 export default ExercisePage;
