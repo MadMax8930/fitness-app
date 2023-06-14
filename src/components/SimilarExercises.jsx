e7aac43c16b4172ad2ffbed7df455612ba9e1a9c
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2023-06-15 01:08:45 +0200

    target & equipment

--- src/components/SimilarExercises.jsx
+++ src/components/SimilarExercises.jsx
@@ -1,7 +1,22 @@
-const SimilarExercises = () => {
-  return (
-    <div>SimilarExo</div>
-  )
-}
+import { Typography, Box, Stack } from '@mui/material';
+
+import { BodyPartCategories, Loader } from '../components';
+
+const SimilarExercises = ({ targetMuscleExercises, equipmentExercises }) => (
+  <Box sx={{ mt: { lg: '100px', xs: '0px' } }}>
+    <Typography sx={{ fontSize: { lg: '44px', xs: '25px' }, ml: '20px' }} fontWeight={700} color="#000" mb="33px">
+      Similar <span style={{ color: '#FF2625', textTransform: 'capitalize' }}>Target Muscle</span> exercises
+    </Typography>
+    <Stack direction="row" sx={{ p: 2, position: 'relative' }}>
+      {targetMuscleExercises.length !== 0 ? <BodyPartCategories data={targetMuscleExercises} /> : <Loader />}
+    </Stack>
+    <Typography sx={{ fontSize: { lg: '44px', xs: '25px' }, ml: '20px', mt: { lg: '100px', xs: '60px' } }} fontWeight={700} color="#000" mb="33px">
+      Similar <span style={{ color: '#FF2625', textTransform: 'capitalize' }}>Equipment</span> exercises
+    </Typography>
+    <Stack direction="row" sx={{ p: 2, position: 'relative' }}>
+      {equipmentExercises.length !== 0 ? <BodyPartCategories data={equipmentExercises} /> : <Loader />}
+    </Stack>
+  </Box>
+);
 
 export default SimilarExercises;
