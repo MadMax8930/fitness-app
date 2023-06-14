0f63b045688852bbd208e4556ca5e144617423f6
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2023-06-15 01:06:50 +0200

    api update

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -2,7 +2,7 @@
    method: 'GET',
    headers: {
      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
-     'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY_1,
+     'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
    },
  };
  
@@ -10,7 +10,7 @@
    method: 'GET',
    headers: {
      'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
-     'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY_2,
+     'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
    },
  };
  
